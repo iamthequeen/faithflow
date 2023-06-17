e327a798dee03cc74d898fcee19a9e68d5a2a9c7
Bianca Simon <biancasimon2112@gmail.com>
2023-06-17 00:44:21 -0500

    Add firestore database export

--- src/utils/firebaseSetup.js
+++ src/utils/firebaseSetup.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
 
 
 // const firebaseConfig = {
@@ -25,3 +26,4 @@ const firebaseConfig = {
 // initialize firebase
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
+export const db = getFirestore(app)
