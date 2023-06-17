23222bd2aa5db73053cab6b62db8d6201e770925
Bianca Simon <biancasimon2112@gmail.com>
2023-06-17 00:38:44 -0500

    Major updates to IdeasPage.jsx

--- src/pages/routes/IdeasPage/IdeasPage.jsx
+++ src/pages/routes/IdeasPage/IdeasPage.jsx
@@ -1,18 +1,40 @@
-import { Box, Grid, Typography, List, ListItem, ListItemText } from '@mui/material';
-import React from 'react';
-import Clock from '../../../components/ui/Clock/Clock';
+import { Box, Grid, Typography, List, ListItem, ListItemText, ListItemButton, useTheme, Stack, Chip } from '@mui/material';
+import React, {useState, useEffect} from 'react';
 import Footer from "../../../components/Footer/Footer";
-
+import IdeasList from "../../../components/IdeasList/IdeasList"
+import Header from '../../../components/Header/Header';
 
 function IdeasPage() {
 
+    const theme = useTheme()
+
+    // useEffect(() => {
+    //     const getIdeasList = async () => {
+    //         try {
+    //             const ideasData = await getDocs(ideasCollectionRef) 
+    //             const filteredIdeasData = ideasData.docs.map(doc => ({...doc.data(), id: doc.id,}))
+    //             // setIdeasList(filteredIdeasData)
+    //             // console.log(filteredIdeasData)
+    //         } catch (err){
+    //             console.error(err)
+    //         }
+    //     }
+
+    //     getIdeasList()
+    // })
+
+    
+
     return (
         <>
+        <Header/>
         <Box
         component="main" 
     sx={{
-      paddingTop: "1.5rem",
-      paddingBottom: "7rem",
+      padding: "7rem 0",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
       minHeight: "100vh",
       textAlign: "center",
       background: `linear-gradient(195.41deg, rgba(186, 207, 255, 0.67) 27.63%, #FFCEB7 74.14%)`,
@@ -21,16 +43,9 @@ function IdeasPage() {
         <Grid container justifyContent="center"
 alignItems="center" direction="column" spacing={4}>
 <Grid item> 
-<Clock/>
+ <IdeasList/> 
 </Grid>
 
-<Grid item>
-<Typography component="h1" variant="h5">Ideas page is coming soon.<br/> Stay Tuned!</Typography>
-</Grid>
-
-<Grid item>
-<Typography variant="body1" color="black">Will contain curated recommendations based on your struggles, habits, and desired improvements.</Typography>
-</Grid>
         </Grid>
         </Box>
         <Footer/>
