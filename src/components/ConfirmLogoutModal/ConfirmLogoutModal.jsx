c416ba5e6f6db076afa4257cddaa4f9356f9a609
Bianca Simon <biancasimon2112@gmail.com>
2023-06-16 22:45:56 -0500

    Add ConfirmLogoutModal.jsx

---
+++ src/components/ConfirmLogoutModal/ConfirmLogoutModal.jsx
@@ -0,0 +1,48 @@
+import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button } from '@mui/material'
+import React, {useContext} from 'react';
+import { UserContext } from "../../utils/UserContext"
+import { useNavigate } from 'react-router-dom';
+
+
+
+function ConfirmLogoutModal(props) {
+const {openConfirmModal, handleCloseModal, setConfirmLogout} = props
+
+    const { logout, setJustLoggedOut} = useContext(UserContext)
+
+    const navigate = useNavigate()
+
+    return (
+         <Dialog
+        open={openConfirmModal}
+        onClose={handleCloseModal}
+        aria-labelledby="confirm-logout-title"
+        aria-describedby="confirm-logout-description"
+      >
+        <DialogTitle id="confirm-logout-title">
+          Confirm Logout
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="confirm-logout-description">
+           Please confirm that you'd like to log out.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseModal}>Cancel</Button>
+          <Button onClick={() => {
+              try {
+                    logout()
+                    setJustLoggedOut(true)
+                    navigate("/logout")
+               } catch(err) {
+                alert(err)
+               }
+            }} autoFocus>
+            Confirm
+          </Button>
+        </DialogActions>
+      </Dialog>
+    )
+}
+
+export default ConfirmLogoutModal
