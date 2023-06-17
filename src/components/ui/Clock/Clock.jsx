f7c9f71ce71f860ef17a23e40b2c9ffe1ee14eb4
Bianca Simon <biancasimon2112@gmail.com>
2023-06-17 00:34:13 -0500

    Shift Clock.jsx to Mui components

--- src/components/ui/Clock/Clock.jsx
+++ src/components/ui/Clock/Clock.jsx
@@ -1,76 +1,18 @@
 import React from 'react'
 import "./Clock.css"
+import { Box } from '@mui/material'
 
 function Clock() {
 
     return (
 
-<div class="clock">
-  <div class="clock__second"></div>
-  <div class="clock__minute"></div>
-    <div class="clock__hour"></div>
-  <div class="clock__axis"></div>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-    <section class="clock__indicator"></section>
-</div>
+<Box class="clock">
+  <Box class="clock__second"></Box>
+  <Box class="clock__minute"></Box>
+    <Box class="clock__hour"></Box>
+  <Box class="clock__axis"></Box>
+  {Array(60).fill(true).map((_, i) => <Box component="section" class="clock__indicator" key={i}></Box>)}
+</Box>
     )
 }
 
