4666aad495394f6550423b9fdbe13eeabd5aca74
Bianca Simon <biancasimon2112@gmail.com>
2023-06-17 00:43:00 -0500

    Add data.js ( with mock data )

---
+++ src/utils/data.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+
+export const ideasList = [
+    {
+        id: 1,
+        mainTopic: "faith",
+        title: "What is faith?",
+        websiteUrl: "www.google.com",
+        tags: ["faith", "gratitude"]
+    },
+    {
+        id: 2,
+        mainTopic: "gratitude",
+        title: "What is gratitude?",
+        websiteUrl: "www.google.com",
+        tags: ["faith", "gratitude", "sin"]
+    },
+    {
+        id: 3,
+        mainTopic: "sin",
+        title: "What is sin?",
+        websiteUrl: "www.google.com",
+        tags: ["faith", "temptation", "gratitude"]
+    },
+    {
+        id: 4,
+        mainTopic: "spiritualGrowth",
+        title: "What is Spiritual Growth?",
+        websiteUrl: "www.google.com",
+        tags: ["faith", "gratitude"]
+    },
+    {
+        id: 5,
+        mainTopic: "tithing",
+        title: "What is tithing?",
+        websiteUrl: "www.google.com",
+        tags: ["faith", "tithing", "gratitude"]
+    },
+    {
+        id: 6,
+        mainTopic: "friendship",
+        title: "What is friendship?",
+        websiteUrl: "www.google.com",
+        tags: ["friendship", "gratitude"]
+    },
+]
