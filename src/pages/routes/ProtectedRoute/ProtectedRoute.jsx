00ba366e4a418a583135adfe895a755d3ce79782
Bianca <88898113+iamthequeen@users.noreply.github.com>
2023-06-24 17:35:11 -0500

    Update ProtectedRoute.jsx

--- src/pages/routes/ProtectedRoute/ProtectedRoute.jsx
+++ src/pages/routes/ProtectedRoute/ProtectedRoute.jsx
@@ -1,27 +1,44 @@
 import React, {useContext} from 'react';
 import { Navigate } from "react-router-dom";
 import { UserContext } from '../../../utils/UserContext';
+import { FormStepContext } from "../../../utils/FormStepContext";
+import { FOOTER_STEPS } from "../../../utils/formSteps";
 import { auth } from '../../../utils/firebaseSetup';
+import {useLocation} from 'react-router-dom'
+import LoadingScreen from "../../../components/ui/LoadingScreen/LoadingScreen";
 
 
 function ProtectedRoute({children}) {
 
     const {
-        guestUser
+        guestUser, currentUser
     } = useContext(UserContext)
 
+     const {
+        setUserStep
+    } = useContext(UserContext)
 
-//   if (!auth?.currentUser || !guestUser) {
-//     return <Navigate to="/" replace />;
-//   }
+    let location = useLocation()
+    //   let from = location.state?.from?.pathname || "/";
 
-//   return children;
 
-  if (auth?.currentUser || guestUser) {
+
+if (currentUser || guestUser ) {
+    // if (location.pathname === "/myhome") {
+    // setUserStep(FOOTER_STEPS.HOME)
+    // }
     return children;
   }
 
-  return <Navigate to="/" replace />;
+//   return <Navigate to="/" replace />;
+return <Navigate to="/" replace />;
+  
+
+//   if (auth?.currentUser || guestUser) {
+//     return children;
+//   }
+
+//   return <Navigate to={from} state={{ from: location }} replace />;
 
 
 }
