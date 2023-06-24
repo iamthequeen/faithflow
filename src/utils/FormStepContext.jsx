dc7123b86c766c6f979590b0114baf4cc48f8adc
Bianca <88898113+iamthequeen@users.noreply.github.com>
2023-06-24 15:55:35 -0500

    Update FormStepContext.jsx

--- src/utils/FormStepContext.jsx
+++ src/utils/FormStepContext.jsx
@@ -1,14 +1,18 @@
 import {createContext, useContext, useState} from "react"
-import { STEPS } from "./formSteps"
+import { STEPS, FOOTER_STEPS } from "./formSteps"
 
 export const FormStepContext = createContext();
 
 export const FormStepContextProvider = ({children}) => {
     const [ formStep, setFormStep ] = useState(STEPS.WELCOME)
 
+        const [ userStep, setUserStep ] = useState(FOOTER_STEPS.HOME)
+
+
     const val = {
         formStep,
-        setFormStep
+        setFormStep,
+        userStep, setUserStep,
     }
 
     return <FormStepContext.Provider value={val}>{children}</FormStepContext.Provider>
